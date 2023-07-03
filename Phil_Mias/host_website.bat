575387b3c67ed14f7944a06eff7a0b4306a3987e
Python Bear <pythonbear@proton.me>
2023-07-03 20:04:36 +0930

    made .bat file open the right URL

--- Phil_Mias/host_website.bat
+++ Phil_Mias/host_website.bat
@@ -1,6 +1,10 @@
 @echo off
 
-start "" http://192.168.1.109:8888
+for /f "tokens=2 delims=:" %%A in ('ipconfig ^| findstr /c:"IPv4 Address"') do set "IP=%%A"
+set "IP=%IP:~1%"
+echo %IP%
+
+start "" http://%IP%:8888
 call "doctor_mias/main.exe"
 
 pause
